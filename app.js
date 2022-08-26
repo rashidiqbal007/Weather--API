9b4d0f96a925363d42c319d2116e76f070aced66
RASHID IQBAL <rashidiqbalf16@gmail.com>
2022-08-26 23:31:35 +0500

    hi

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+//body parser allows to look thru the body of the post request.(accessed by name given in input)
 const { Http2ServerRequest } = require("http2");
 
 
-const app = express()
+
+
+const app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
     app.get("/" , function(req,res){
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=68e0785a018c559ba5c627e1cbd5d8f4&units=metric"
+      
+        res.sendFile(__dirname + "/index.html")
+    })
+
+
+    // post request
+    app.post("/" , function(req,res){
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=68e0785a018c559ba5c627e1cbd5d8f4&units=metric"
         
         https.get(url , function(response){
-
-            console.log(response);
+        console.log(response.statusCode);
             
+        response.on("data", function(data){
+            // console.log(data);
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+
+             const temp = weatherData.main.temp;
+            // console.log(temp);
+
+            const  cityDescription = weatherData.weather[0].description;
+
+            const wind = weatherData.wind.speed;
+            // console.log(wind);
+
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png"
+
+            res.write("<p>The wind speed in" + query + " is " + wind + "</p>");
+            res.write("The weather in "+ query + " is " + cityDescription);
+            res.write("The temprature is currently "+ temp)
+
+            res.write("<img src = " + imageURL + ">");
+            res.send();
 
         })
-        res.send("hi i am alive")
+
+        })
+        // console.log("received");
     })
 
 
@@ -23,3 +60,9 @@ const app = express()
         console.log("Server running on port 3000");
     })
     
+
+
+
+
+
+   
